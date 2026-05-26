d6c275f7bc17fc326ddfae208e569d56399c848a
landry68 <landryshema63@gmail.com>
2026-05-26 09:47:41 +0200

    my first commit on first project

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script setup>
+</script>
+
+<template>
+  <div class="min-h-screen bg-blue-50 p-8">
+
+    <!-- Header -->
+    <header class="text-center mb-8">
+
+      <!-- Logo -->
+      <div class="flex justify-center mb-4">
+        <img
+          src="./assets/logo-sosve-sans-baseline-4.png"
+          alt="SOS Logo"
+          class="w-24 h-12"
+        />
+      </div>
+
+      <h1 class="text-5xl font-bold text-blue-700">
+        SOS Children's Village School
+      </h1>
+
+      <p class="text-gray-700 mt-2 text-lg">
+        Learning Together For A Better Future
+      </p>
+    </header>
+
+    <!-- Main Image -->
+    <div class="flex justify-center mb-6">
+      <img
+        src="./assets/123456.png"
+        alt="Students"
+        class="rounded-4xl shadow-lg"
+      />
+    </div>
+
+    <!-- Text Content -->
+    <div class="max-w-4xl mx-auto text-gray-800 text-lg leading-8">
+
+      <h2 class="text-3xl font-semibold text-blue-700 mb-2">
+        About The School
+      </h2>
+
+      <p class="mb-4">
+        SOS Children's Village School is a place where children receive quality
+        education in a safe and caring environment. The school helps students
+        learn, grow, and prepare for a successful future.
+      </p>
+
+      <p class="mb-4">
+        Students study different subjects such as Mathematics, Science,
+        Languages, and ICT. Teachers guide students during lessons and help
+        them improve their knowledge and skills every day.
+      </p>
+
+      <p class="mb-4">
+        The school also encourages teamwork, discipline, sports, creativity,
+        and leadership activities so that students can develop their talents
+        and confidence.
+      </p>
+
+      <p class="mb-4">
+        SOS Children's Village works together with families and the community
+        to support children and provide them with better opportunities in life.
+      </p>
+
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+body {
+  font-family: Arial, sans-serif;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+ import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailWindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailWindcss(),
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
